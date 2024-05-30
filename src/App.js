f91e0f326891bf0d8943392f4a749dc787a3e077
salmanansari.14 <salmanansari1409@gmail.com>
2024-05-31 01:55:45 +0530

    kg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Link
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          to="/home"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Link>
+          
       </header>
-      <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home />} />
         </Routes>
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Homefgfg</div>
+    <div>Homefgfg
+        <Link to='/home'>ghrgh</Link>
+    </div>
   )
 }
 
